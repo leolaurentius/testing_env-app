7798d8788e7230092456d57de8c4fcbc47631156
Lorenzo <lorenzo@lobba.it>
2017-01-16 12:10:10 +0100

    add addUserTest page testing

---
+++ README.md
@@ -0,0 +1,32 @@
+# Test of an nodejs, Reactos, express and express-stormpath application
+
+#### 1. Launch selenium GRID (version 3.0.1)
+```bash
+$ docker run -d -p 4444:4444 --name selenium-hub selenium/hub:3.0.1-aluminum
+$ docker run -d --link selenium-hub:hub selenium/node-chrome:3.0.1-aluminum
+$ docker run -d --link selenium-hub:hub selenium/node-firefox:3.0.1-aluminum
+```
+#### 2. Launch Web application
+```bash
+$ cd <root directory of WebApplication>
+$ npm install (only first time)
+$ npm start
+```
+#### 3. Launch Test
+```bash
+mvn test
+```
+
+## HowTo Install Oracle Java on Ubuntu/ Debian
+
+###### [Istituto Majorana. Howto install java](http://www.istitutomajorana.it/index.php?option=com_content&task=view&id=2148&Itemid=33) (Italian Doc)
+###### [HowTo intall java](https://www.digitalocean.com/community/tutorials/how-to-install-java-on-ubuntu-with-apt-get) (English Doc)
+
+```bash
+sudo apt-get install python-software-properties
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+# I use java 8 (Java 9 is already available, but it is an early version)
+sudo apt-get install oracle-java8-installer
+sudo update-alternatives --config java
+```
---
+++ src/test/java/com/moxconsulting/testing/addUserTest.java
@@ -0,0 +1,104 @@
+package com.moxconsulting.testing;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import java.net.*;
+import java.io.File;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.annotations.*;
+
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+// for local firefox
+// import org.openqa.selenium.firefox.FirefoxDriver;
+// import org.openqa.selenium.firefox.FirefoxBinary;
+// import org.openqa.selenium.firefox.FirefoxProfile;
+// END FOR LOCAL FIREFOX
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class addUserTest {
+private WebDriver driver;
+private String baseUrl;
+private boolean acceptNextAlert = true;
+private StringBuffer verificationErrors = new StringBuffer();
+
+@BeforeSuite
+public void setUp() throws Exception {
+        // For GRID
+        String hubNodeGridUrl = "http://localhost:4444/wd/hub";
+        DesiredCapabilities capability = new DesiredCapabilities();
+        try {
+                // For local firefox
+                // driver = new FirefoxDriver();
+                // For GRID
+                driver = new RemoteWebDriver(new URL(hubNodeGridUrl), DesiredCapabilities.firefox());
+        }
+        catch (Exception e) {
+                System.out.println(e);
+        }
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        // For local firefox
+        // baseUrl = "http://localhost:3000";
+        // For GRID
+        baseUrl = "http://172.17.0.1:3000";
+}
+
+@Test(description="Create new user.")
+public void createUser() throws Exception {
+        driver.get(baseUrl + "/");
+
+        driver.findElement(By.linkText("Registration")).click();
+        driver.findElement(By.id("givenName")).sendKeys("Lorenzo");
+        driver.findElement(By.id("surname")).click();
+        driver.findElement(By.id("surname")).sendKeys("Lobba");
+        driver.findElement(By.id("email")).sendKeys("lorenz@linux.it");
+        driver.findElement(By.id("customData.color")).sendKeys("blue");
+        driver.findElement(By.id("password")).sendKeys("Aa123456789");
+        driver.findElement(By.cssSelector("button.btn.btn-primary")).click();
+
+        try {
+                String ans = "Welcome Lorenzo!";
+                Assert.assertEquals(driver.findElement(By.cssSelector("h2.text-center")).getText(), ans);
+        } catch (Error e) {
+                verificationErrors.append(e.toString());
+        }
+        driver.findElement(By.linkText("Logout")).click();
+}
+
+@Test(description="Try to create new user. Already created")
+public void createUserAgain() throws Exception {
+        driver.get(baseUrl + "/");
+        driver.findElement(By.linkText("Registration")).click();
+        driver.findElement(By.id("givenName")).sendKeys("Lorenzo");
+        driver.findElement(By.id("surname")).click();
+        driver.findElement(By.id("surname")).sendKeys("Lobba");
+        driver.findElement(By.id("email")).sendKeys("lorenz@linux.it");
+        driver.findElement(By.id("customData.color")).sendKeys("blue");
+        driver.findElement(By.id("password")).sendKeys("Aa123456789");
+        driver.findElement(By.cssSelector("button.btn.btn-primary")).click();
+
+        try {
+                String ans = "Account with that email already exists. Please choose another email.";
+                Assert.assertEquals(driver.findElement(By.cssSelector("p.alert-danger")).getText(), ans);
+        } catch (Error e) {
+                verificationErrors.append(e.toString());
+        }
+}
+
+
+@AfterSuite
+public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+                Assert.fail(verificationErrorString);
+        }
+}
+}
